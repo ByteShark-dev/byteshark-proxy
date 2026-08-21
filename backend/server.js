7f65f12bd32e32bee5e569693ccea0d67a77fd9e
Angel Arenas Patiño <byteshark098@gmail.com>
2026-08-21 08:46:45 -0600

    Si

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import wisp from '@mercuryworkshop/wisp-server-node';
+
+const bareServer = createBareServer('/bare/');
+const app = express();
+const server = createServer();
+
+server.on('request', (req, res) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (req.url.endsWith('/wisp/')) {
+        wisp.routeRequest(req, socket, head);
+    } else if (bareServer.shouldRoute(req)) {
+        bareServer.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+app.get('/', (req, res) => res.send('ByteShark Proxy Backend: Online'));
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`[CORE] Running on port ${PORT}`));
